a6ab066c27c7dde96b7f50b4269dd1f7064e6cfa
JacopoLombardi <lombajacopo@gmail.com>
2024-02-28 15:59:24 +0100

    [add] Pari/Dispari Task 5 completed

--- Pari  o Dispari/js/script.js
+++ Pari  o Dispari/js/script.js
@@ -10,10 +10,14 @@ const userNumber = 2   //prompt('inserisci un numero da 1 a 5');
 const pcNumber = randomNumber(1, 6);
 console.log(pcNumber);
 
+
 // somma del numero User e numero Pc
 const totale = userNumber + pcNumber;
 
-console.log(totale)
+
+// invoco la funzione che stabilisce se il totale è Pari o Dispari
+const result = totalePariDispari(totale);
+console.log('result', result);
 
 
 
@@ -21,23 +25,19 @@ console.log(totale)
 
 
 
-///////  FUNCTIONS  //////////
+/////////  FUNCTIONS  //////////
 
 // funzione randomNumber
 function randomNumber(min, max){
    return Math.floor(Math.random() * (max - min) + min);
 }
 
-
-
-function totalePariDispari(totale){
-
-
-
-
-
+// funzione per stabilire se la somma è Pari o Dispari
+function totalePariDispari(numTot){ 
+   if((numTot % 2) === 0){
+      return 'pariii';
+   }
+   else{
+      return 'dispariii';
+   }
 }
-
-
-
-
