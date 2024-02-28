235f7e7d9fb8a276a6b3a0bfa234c0bf789c4bfc
JacopoLombardi <lombajacopo@gmail.com>
2024-02-28 19:48:29 +0100

    [add] Palindroma maybe works, but on last For there's any problem

--- Palindroma/README.md
+++ Palindroma/README.md
@@ -1,5 +1,8 @@
 Palindroma
 ===
 1. Inserire la Parola.
-2. ???
-3. 
+2. Controllare da quante lettere è composta la stringa.
+3. tramite un ciclo For e all'aiuto di '.charAt()' selezioniamo lettera per lettera e le spostiamo in un Array vuoto.
+4. Creiamo un altro Array, ma sarà esattamente al contrario di quello creato prima.
+5. Adesso controlliamo dentro un ciclo For, lettera per lettera entrambi gli Array.
+6. Se trovo una lettera diversa dall'altra, vuol dire che la parola inserita NON è Palindroma, altrimente è Palindroma.
--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,6 +1,12 @@
 
-// variabile collegata al btn_check dell'HTML
+const emptyArray = [];
+const copyReverseArray = [];
+
+
+// variabile collegata al .btn_check dell'HTML
 const btnCheck = document.querySelector('.btn_check');
+const outputResult = document.querySelector('.output_result');
+
 
 // quando viene cliccato il pulsante...
 btnCheck.addEventListener('click', function(){
@@ -8,14 +14,52 @@ btnCheck.addEventListener('click', function(){
    // leggi il valore dell'input
    const inputText = document.querySelector('.input_text').value;
 
-   console.log(inputText);
+   // leggo da quante lettere è formata la stringa
+   const inputTextLength = inputText.length;
 
+
+
+   for(let i = 0; i < inputTextLength; i++){
+      // mi permette di prendere da una stringa una lettera alla volta '.charAt()'
+      const lettera = inputText.charAt(i);
+
+      // inserisco una ad una le lettere nell' emptyArray
+      emptyArray.push(lettera);
+   }
+
+   console.log(emptyArray)
+
+
+
+   // variabile che conta esattamente al contrario di 'i' nel For
+   let iReverse = inputTextLength-1;
+
+   // copiamo emptyArray in copyReverseArray, però al contrario
+   for(let i = 0; i < inputTextLength; i++){
+      
+      copyReverseArray[iReverse] = emptyArray[i];
+      iReverse--;
+   }
+
+   console.log(copyReverseArray)
+
+
+   
+   // confronto incrociato tra due Array, uno parte dall'inizio, mentre l'altro parte dalla fine
+   for(let i = 0; i < inputTextLength; i++){
+
+      // due variabili per confrontare ogni singola lettera dei 2 Array contemporaneamente
+      const letteraEmptyArray = emptyArray[i];
+      const letteraCopyReverseArray = copyReverseArray[i];
+      
+      // se la lettera del primo e del secondo Array sono diverse...
+      if(letteraEmptyArray != letteraCopyReverseArray){
+         outputResult.innerHTML = `La parola inserita NON è Palindroma`;
+      }
+   }
+
+   outputResult.innerHTML = `La parola inserita è Palindroma`;
 
 })
-
-
-
-
-
 
 
