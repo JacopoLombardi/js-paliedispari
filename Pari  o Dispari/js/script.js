bb4e9eec1fbeae8f2e5f1bd30fb91a3948add2b2
JacopoLombardi <lombajacopo@gmail.com>
2024-02-28 17:21:07 +0100

    [add] Pari/Dispari Finished

--- Pari  o Dispari/js/script.js
+++ Pari  o Dispari/js/script.js
@@ -1,28 +1,43 @@
 
-
-const userPariDispari = 'pari'   //prompt('scrivere "pari" oppure "dispari"');
-
-const userNumber = 2   //prompt('inserisci un numero da 1 a 5');
-
-
-
-// invoco la funzione randomNumber
-const pcNumber = randomNumber(1, 6);
-console.log(pcNumber);
-
-
-// somma del numero User e numero Pc
-const totale = userNumber + pcNumber;
-
-
-// invoco la funzione che stabilisce se il totale è Pari o Dispari
-const result = totalePariDispari(totale);
-console.log('result', result);
+// associo le variabili Js alle classi dell'HTML
+const btnPlay = document.querySelector('.btn_play');
+const pcNumber_out = document.querySelector('.pc_numout');
+const output = document.querySelector('.output_');
 
 
 
+// quando viene cliccato btnPlay....
+btnPlay.addEventListener('click', function(){
+
+   // leggo il valore input User di pari o dispari
+   const userPariDispari = document.querySelector('.user_paridispari').value;
 
 
+   // leggo il valore input User da 1 a 5
+   const userNumber = parseInt(document.querySelector('.user_number').value);
+
+
+   // invoco la funzione randomNumber inserendo valore 'min' e 'max'
+   const pcNumber = randomNumber(1, 6);
+   pcNumber_out.innerHTML = `numero di Pc: ${pcNumber}`;
+
+
+   // somma del numero User e numero Pc
+   const totale = userNumber + pcNumber;
+
+
+   // invoco la funzione che stabilisce se il totale è Pari o Dispari
+   const result = totalePariDispari(totale);
+
+
+   if((userPariDispari === 'pari') && (result === 'pari')  ||  (userPariDispari === 'dispari') && (result === 'dispari')){
+      output.innerHTML = 'Vince User!'
+   }
+   else{
+      output.innerHTML = 'Vince Pc!'
+   }
+
+})
 
 
 /////////  FUNCTIONS  //////////
@@ -35,9 +50,9 @@ function randomNumber(min, max){
 // funzione per stabilire se la somma è Pari o Dispari
 function totalePariDispari(numTot){ 
    if((numTot % 2) === 0){
-      return 'pariii';
+      return 'pari';
    }
    else{
-      return 'dispariii';
+      return 'dispari';
    }
 }
