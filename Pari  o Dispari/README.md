ac7c9c508560ceb552d12bf8f39b9386a2718742
JacopoLombardi <lombajacopo@gmail.com>
2024-02-28 15:48:31 +0100

    [add] Pari/Dispari Task 4 completed

--- Pari  o Dispari/README.md
+++ Pari  o Dispari/README.md
@@ -3,5 +3,6 @@ Pari o Dispari
 1. User sceglie Pari o Dispari.
 2. User scrive un numero da 1 a 5.
 3. Generiamo un numero random per il Pc, tramite Funzione.
-4. 
-5. 
+4. Sommiamo il numero dell'User e del Pc.
+5. Stabiliamo se la somma è Pari oppure Dispari tramite Funzione.
+6. Stabilire il Vincitore.
--- Pari  o Dispari/js/script.js
+++ Pari  o Dispari/js/script.js
@@ -5,19 +5,39 @@ const userPariDispari = 'pari'   //prompt('scrivere "pari" oppure "dispari"');
 const userNumber = 2   //prompt('inserisci un numero da 1 a 5');
 
 
+
+// invoco la funzione randomNumber
 const pcNumber = randomNumber(1, 6);
-
-
 console.log(pcNumber);
+
+// somma del numero User e numero Pc
+const totale = userNumber + pcNumber;
+
+console.log(totale)
+
+
+
 
 
 
 
 ///////  FUNCTIONS  //////////
 
+// funzione randomNumber
 function randomNumber(min, max){
    return Math.floor(Math.random() * (max - min) + min);
 }
+
+
+
+function totalePariDispari(totale){
+
+
+
+
+
+}
+
 
 
 
