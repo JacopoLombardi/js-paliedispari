a8f133b63ea382bd0ceb66ccf93b27d4c5253671
JacopoLombardi <lombajacopo@gmail.com>
2024-02-28 15:34:51 +0100

    [add] Pari/Dispari, Task 1-2 and 3 completed

--- Pari  o Dispari/js/script.js
+++ Pari  o Dispari/js/script.js
@@ -1,16 +1,23 @@
 
 
+const userPariDispari = 'pari'   //prompt('scrivere "pari" oppure "dispari"');
+
+const userNumber = 2   //prompt('inserisci un numero da 1 a 5');
+
+
+const pcNumber = randomNumber(1, 6);
+
+
+console.log(pcNumber);
 
 
 
 
+///////  FUNCTIONS  //////////
 
-
-
-
-
-
-
+function randomNumber(min, max){
+   return Math.floor(Math.random() * (max - min) + min);
+}
 
 
 
